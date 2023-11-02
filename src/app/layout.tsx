bb32284faca9724de38e88cda68b06ab080a91eb
Guilherme-07062002 <guilherme762002@gmail.com>
2023-11-02 15:44:51 -0300

    fix: font adjust

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,4 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "To-do List",
@@ -15,7 +12,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
